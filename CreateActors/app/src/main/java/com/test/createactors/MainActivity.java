42d3cfa221d8bd0e4948f1ad2ce9530e693ad47e
jon k <jonkk.88@gmail.com>
2016-07-25 12:13:40 -0700

    added mainactivity

--- CreateActors/app/src/main/java/com/test/createactors/MainActivity.java
+++ CreateActors/app/src/main/java/com/test/createactors/MainActivity.java
@@ -2,12 +2,29 @@ package com.test.createactors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView mListView;
+    ArrayList<Actor> actors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        actors = new ArrayList<>();
+        actors.add(new Actor("Shaq", "March 6, 1972", 0));
+        actors.add(new Actor("Will Smith", "September 25, 1968", 0));
+        actors.add(new Actor("Christopher Waltz", "October 4, 1956", 0));
+
+        ActorArrayAdapter actorArrayAdapter = new ActorArrayAdapter(actors, MainActivity.this);
+
+        mListView.setAdapter(actorArrayAdapter);
+
     }
 }
